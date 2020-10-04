e7534c0426dd6518b048651a6f064329462ce7e4
mattmcv <23mcvicker@gmail.com>
2020-10-03 19:56:38 -0700

    Add some marker

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
     "use strict";
-  
+
     /**
      * The Globe encapsulates the WorldWindow object (wwd) and provides application
      * specific logic for interacting with layers.
@@ -8,87 +8,103 @@ $(document).ready(function() {
      * @returns {Globe}
      */
     class Globe {
-      constructor(canvasId) {
-        // Create a WorldWindow globe on the specified HTML5 canvas
-        this.wwd = new WorldWind.WorldWindow(canvasId);
-  
-        // Holds the next unique id to be assigned to a layer
-        this.nextLayerId = 1;
-  
-        // Add a BMNGOneImageLayer background layer. We're overriding the default 
-        // minimum altitude of the BMNGOneImageLayer so this layer always available.
-        this.addLayer(new WorldWind.BMNGOneImageLayer(), {
-          category: "background",
-          minActiveAltitude: 0
-        });
-      }
-  
-      /**
-       * Adds a layer to the globe. Applies the optional options' properties to the
-       * layer, and assigns the layer a unique ID and category.
-       * @param {WorldWind.Layer} layer
-       * @param {Object|null} options E.g., {category: "base", enabled: true}
-       */
-      addLayer(layer, options) {
-        // Copy all properties defined on the options object to the layer
-        if (options) {
-          for (let prop in options) {
-            if (!options.hasOwnProperty(prop)) {
-              continue; // skip inherited props
+        constructor(canvasId) {
+            // Create a WorldWindow globe on the specified HTML5 canvas
+            this.wwd = new WorldWind.WorldWindow(canvasId);
+
+            // Holds the next unique id to be assigned to a layer
+            this.nextLayerId = 1;
+
+            // Add a BMNGOneImageLayer background layer. We're overriding the default 
+            // minimum altitude of the BMNGOneImageLayer so this layer always available.
+            this.addLayer(new WorldWind.BMNGOneImageLayer(), {
+                category: "background",
+                minActiveAltitude: 0
+            });
+        }
+
+        /**
+         * Adds a layer to the globe. Applies the optional options' properties to the
+         * layer, and assigns the layer a unique ID and category.
+         * @param {WorldWind.Layer} layer
+         * @param {Object|null} options E.g., {category: "base", enabled: true}
+         */
+        addLayer(layer, options) {
+            // Copy all properties defined on the options object to the layer
+            if (options) {
+                for (let prop in options) {
+                    if (!options.hasOwnProperty(prop)) {
+                        continue; // skip inherited props
+                    }
+                    layer[prop] = options[prop];
+                }
             }
-            layer[prop] = options[prop];
-          }
+            // Assign a default category property if not already assigned
+            if (typeof layer.category === 'undefined') {
+                layer.category = 'overlay'; // the default category
+            }
+
+            // Assign a unique layer ID to ease layer management 
+            layer.uniqueId = this.nextLayerId++;
+
+            // Add the layer to the globe
+            this.wwd.addLayer(layer);
         }
-        // Assign a default category property if not already assigned
-        if (typeof layer.category === 'undefined') {
-          layer.category = 'overlay'; // the default category
+
+        /**
+         * Returns a new array of layers in the given category.
+         * @param {String} category E.g., "base", "overlay" or "setting".
+         * @returns {Array}
+         */
+        getLayers(category) {
+            return this.wwd.layers.filter(layer => layer.category === category);
         }
-  
-        // Assign a unique layer ID to ease layer management 
-        layer.uniqueId = this.nextLayerId++;
-  
-        // Add the layer to the globe
-        this.wwd.addLayer(layer);
-      }
-  
-      /**
-       * Returns a new array of layers in the given category.
-       * @param {String} category E.g., "base", "overlay" or "setting".
-       * @returns {Array}
-       */
-      getLayers(category) {
-        return this.wwd.layers.filter(layer => layer.category === category);
-      }
     }
-  
+
     // Create a globe
     let globe = new Globe("globe-canvas");
-  
+
     // Add layers to the globe 
     globe.addLayer(new WorldWind.BMNGLayer(), {
-      category: "base"
+        category: "base"
     });
     globe.addLayer(new WorldWind.CoordinatesDisplayLayer(globe.wwd), {
-      category: "setting"
+        category: "setting"
     });
     globe.addLayer(new WorldWind.ViewControlsLayer(globe.wwd), {
-      category: "setting"
+        category: "setting"
     });
     globe.addLayer(new WorldWind.CompassLayer(), {
-      category: "setting",
-      enabled: false
+        category: "setting",
+        enabled: false
     });
     globe.addLayer(new WorldWind.StarFieldLayer())
-  
-  
+
+    //create satellite layer
+
+    var layer = new WorldWind.RenderableLayer("placeMarks");
+    var placemark = new WorldWind.Placemark(new WorldWind.Position(100, 100, 300));
+    layer.addRenderable(placemark);
+    globe.addRenderable(layer)
+
+    
+    // var kmlFilePromise = new WorldWind.KmlFile('data/KML_Samples.kml');
+    // kmlFilePromise.then(function (kmlFile) {
+    //     layer.addRenderable(kmlFile);
+
+    //     wwd.addLayer(renderableLayer);
+    // });
+
+
+
+
     // Auto-collapse the main menu when its button items are clicked
-    $('.navbar-collapse a[role="button"]').click(function() {
-      $('.navbar-collapse').collapse('hide');
+    $('.navbar-collapse a[role="button"]').click(function () {
+        $('.navbar-collapse').collapse('hide');
     });
-  
+
     // Collapse card ancestors when the close icon is clicked
-    $('.collapse .close').on('click', function() {
-      $(this).closest('.collapse').collapse('hide');
+    $('.collapse .close').on('click', function () {
+        $(this).closest('.collapse').collapse('hide');
     });
-  });
-  
+});
